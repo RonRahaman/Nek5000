07679c51d98143ce1e5d4c1cdf6100c7353f2f47
Ron Rahaman <rahaman@gmail.com>
2016-11-23 11:38:38 -0600

    Commented nekFileConfig.py

--- short_tests/lib/nekFileConfig.py
+++ short_tests/lib/nekFileConfig.py
@@ -2,19 +2,25 @@ import os, stat, re
 
 
 def config_makenek(opts, infile, outfile):
-    """ Redefine variables in a makenek file
+    """ Configure a makenek file by redefining the variables therein
 
-    Given a path to infile, redefine the variables & values in opts, then
-    output to outfile.  The infile and outfile can be the same file.
+    Given a path to an input makenek file ('infile'), set the variables and corresponding values from the dictionary
+    'opts'.  Output the resultant makenek to 'outfile'. If 'infile' and 'outfile' are the same file, then the infile
+    will be modified in-place.  Any variables not listed in 'opts' will be copied from 'infile' to 'outfile' without
+    modification.
 
-    Only the options already available in makenek may be set.  This function
-    doesn't allow you to declare arbitrary variables in makenek.
-    (TODO: Raise warning when attempting to set an unavailable variable.)
+    For example, this creates a new makenek file (makenek.new) where F77, CC and G are set to pgf77, pgcc, and -g,
+    respectively:
+    config_makenek({'F77':'pgf77', 'CC':'pgcc 'G':'-g'} '/home/rahaman/Nek5000/core/makenek', '/home/rahaman/makenek.new')
+
+    Only the options already available in makenek may be set.  This function doesn't allow you to define any new
+    variables in makenek. (TODO: Raise warning when attempting to define a new variable.)
 
     Args:
-        opts ({variable : value}): Set each "variable=value" in makenek
-        infile (): The input makenek file
-        outfile (): The output makenek file
+        opts ({variable : value, ...}): Set each "variable=value" in the
+                                        resultant makenek
+        infile (str): Path to the input makenek file
+        outfile (str): Path to the resultant makenek file
 
     """
     with open(infile, 'r') as f:
@@ -38,6 +44,21 @@ def config_makenek(opts, infile, outfile):
 
 
 def config_maketools(infile, outfile, f77=None, cc=None, bigmem=None):
+    """ Configure a maketools file by redefining the variables therein
+
+    Given a path to an input maketools file ('infile'), redefine F77, CC, and BIGMEM to the values provided by 'f77',
+    'cc', and 'bigmem'.  Output the resultant makenek to 'outfile'. If 'f77', 'cc', or 'bigmem' are empty or None,
+    then the corresponding variables will be copied from 'infile' to 'outfile without modification. If infile and
+    outfile are the same file, then the infile will be reformatted in-place.
+
+    Args:
+        infile (str): Path to the input maketools file
+        outfile (str): Path to the resultant maketools file
+        f77 (str): Value for F77 variable in maketools
+        cc (str): Value for CC variable in maketools
+        bigmem (str): Value for BIGMEM variable in maketools
+
+    """
     with open(infile, 'r') as f:
         lines = f.readlines()
 
@@ -60,6 +81,16 @@ def config_maketools(infile, outfile, f77=None, cc=None, bigmem=None):
 
 
 def config_basics_inc(infile, outfile, nelm):
+    """ Configure a basics.inc file by redefining nelm
+
+    If 'infile' and 'outfile' are the same, then 'infile' will be reformatted in-place
+
+    Args:
+        infile (str): Path to the input basics.inc file
+        outfile (str): Path to the resultant basics.inc file
+        nelm (str|int): Number of elements
+
+    """
     with open(infile, 'r') as f:
         lines = f.readlines()
 
@@ -71,18 +102,18 @@ def config_basics_inc(infile, outfile, nelm):
 
 
 def config_size(params, infile, outfile):
-    """ Redefine parameters in a SIZE file
+    """ Configure a SIZE file by redefining the variables therein
 
-    Given a path to infile, redefine the variables & values in params, then
-    output to outfile.  The infile and outfile can be the same file.
+    Given a path to an input SIZE file ('infile'), redefine each parameter:value pair from the dictionary 'opts'.
+    Output the resultant SIZE file to 'outfile'.  Any parameters from 'infile' that aren't listed in 'opts' will
+    be copied from infile to outfile without modification. If infile and outfile are the same file, then
+    the infile will be reformatted in-place.
 
-    Only the parameters already declared in infile may be set.  This function
-    doesn't allow you to declare any new parameters.
-    (TODO: Raise warning when attempting to declare a new parameter.)
+    Only the parameters already defined in infile may be set.  This function doesn't allow you to define any new
+    parameters. (TODO: Raise warning when attempting to define a new parameter.)
 
     Args:
-        params ({variable : value}): Each 'variable' will be set to 'value'
-            in the output SIZE file
+        params ({variable : value, ...}): Each 'variable' will be set to 'value' in the output SIZE file
         infile (str): Path to the input SIZE file
         outfile (str): Path to output SIZE file
 
@@ -104,22 +135,21 @@ def config_size(params, infile, outfile):
 
 
 def config_parfile(opts, infile, outfile):
-    """ Set values in a parfile using ConfigParser
+    """ Configure a .par file by redefining the options therein.
 
-    Given a path to infile, substitute the options & values in opts, then
-    output to outfile.  The infile and outfile can be the same file.
+    Given a path to an input .par file ('infile'), set all the options in the dictionary 'opts' (see below for
+    explanation of dictionary format).  If infile and outfile are the same file, then the .par file will be modified
+    in-place.
 
     opts is interpreted as a nested dict of the form:
         {section: {optname: value, ...}, ...}
-    where "optname = value" are set in [section]. If 'optname' is not set in
-    infile, then it will be added to outfile.  If 'optname' is already set in
-    infile, then it will be overridden in outfile.  If an option is listed in
-    infile but is not listed in in 'opts', then it will be copied to outfile
-    without modification..
+    where "optname = value" are set in [section].
+    * If 'optname' is not set in infile, then it will be added to outfile.
+    * If 'optname' is already set in infile, then it will be overridden in outfile.
+    * If an option is listed in infile but is not listed in in 'opts', then it will be copied unmodified to outfile.
 
     Args:
-        opts ({section: {optname : value}}): Set each "optname = value" in
-            each "[section]"
+        opts ({section: {optname : value, ...}, ...}): Set each "optname = value" in the corresponding "[section]"
         infile (str): Path to input parfile
         outfile (str): Path to output parfile
 
