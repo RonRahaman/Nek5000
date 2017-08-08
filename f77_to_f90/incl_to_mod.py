736455e9a080e5cff13a255656fbbe655a108f4d
Ron Rahaman <rahaman@gmail.com>
2017-08-08 15:14:46 -0500

    incl_to_mod script adds implicit none

--- f77_to_f90/incl_to_mod.py
+++ f77_to_f90/incl_to_mod.py
@@ -1,17 +1,32 @@
-import os
+import os, subprocess
 
 for infileName in os.listdir():
 
-    if infileName.upper() == infileName:
+    if infileName.upper() == infileName and infileName != 'HEADER':
+
         moduleName =  infileName.lower()
         outfileName = "global_{}.F90".format(moduleName)
-        #with open(infileName, '+') as infile, open(outfileName, 'w') as outFile:
+
+        # Read file contents
         with open(infileName, 'r') as infile:
             inputLines = infile.readlines()
-            outputLines = ['      module {0}\n'.format(moduleName)] \
-                          + inputLines \
-                          + ['      end module {0}\n'.format(moduleName)]
-        with open(infileName, 'w') as outfile:
+
+        # Prepend/append module declarations around file contents
+        header = [
+            '      module {0}\n'.format(moduleName),
+            '      use global_size\n',
+            '      implicit none\n',
+        ]
+        footer = [
+            '      end module {0}\n'.format(moduleName)
+        ]
+        outputLines = header + inputLines + footer
+
+        # Rename the file
+        subprocess.run(['git', 'mv', infileName, outfileName])
+
+        # Replace the file contents
+        with open(outfileName, 'w') as outfile:
             outfile.writelines(outputLines)
 
 
