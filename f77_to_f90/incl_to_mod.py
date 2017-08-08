3faa7e3116b8bdaf45e70f7a0d1ab7167b87c46c
Ron Rahaman <rahaman@gmail.com>
2017-08-08 15:48:51 -0500

    incl_to_mod changes "include" statements to "use" statements

--- f77_to_f90/incl_to_mod.py
+++ f77_to_f90/incl_to_mod.py
@@ -1,11 +1,11 @@
-import os, subprocess
+import os, subprocess, re
 
 for infileName in os.listdir():
 
     if infileName.upper() == infileName and infileName != 'HEADER':
 
-        moduleName =  infileName.lower()
-        outfileName = "global_{}.F90".format(moduleName)
+        outfileName = "global_{}.F90".format(infileName.lower())
+        moduleName =  "GLOBAL_{}".format(infileName.upper())
 
         # Read file contents
         with open(infileName, 'r') as infile:
@@ -14,7 +14,7 @@ for infileName in os.listdir():
         # Prepend/append module declarations around file contents
         header = [
             '      module {0}\n'.format(moduleName),
-            '      use global_size\n',
+            '      use GLOBAL_SIZE\n',
             '      implicit none\n',
         ]
         footer = [
@@ -22,8 +22,14 @@ for infileName in os.listdir():
         ]
         outputLines = header + inputLines + footer
 
+        outputLines =  [ re.sub(
+            r'\s*include (?:\'|")(?!HEADER)(\w+)(?:\'|")',
+            r'      use GLOBAL_\g<1>',
+            line
+        ) for line in outputLines ]
+
         # Rename the file
-        subprocess.run(['git', 'mv', infileName, outfileName])
+        # subprocess.run(['git', 'mv', infileName, outfileName])
 
         # Replace the file contents
         with open(outfileName, 'w') as outfile:
