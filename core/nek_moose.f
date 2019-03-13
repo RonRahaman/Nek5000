c9ef68474644e1685a9722e8dcb5c48e14063097
Elia Merzari <merzari@hall.mcs.anl.gov>
2019-03-12 19:55:04 -0500

    Adding flux reconstruction routines

--- core/nek_moose.f
+++ core/nek_moose.f
@@ -136,20 +136,6 @@ c-----------------------------------------------------------------------
 
       return
       end
-c-----------------------------------------------------------------------
-      subroutine rassign (fu,vpf,nn)
-      include 'SIZE'
-      include 'TOTAL'
-      include 'NEKMOOSE'
-      integer e,eg
-
-      real*8 vpf(lsurf_m*4)
-      real*8 fu(3,3,3,lelt)
-
-c     Placeholder
-
-      return
-      end
 C=======================================================================
       subroutine nek_interpolation()
       include 'SIZE'
@@ -162,32 +148,6 @@ C=======================================================================
       return
       end
 C=======================================================================
-      subroutine flux_reconstruction()
-      include 'SIZE'
-      include 'TOTAL'
-      include 'NEKMOOSE'
-      integer e,f
-      ntot=nx1*ny1*nz1*nelt
-
-!     Call to rassign and spectral interpolation
-      do e=1,nelt
-        do f=1,6
-          call surface_int(sint,sarea,flux_recon,e,f)
-          if (cbc(f,e,1).eq.'W  ') then
-           sint1=sint1+sint
-           sarea1=sarea1+sarea
-          endif
-         enddo
-      enddo
-
-      call  gop(sint1,wtmp,'+  ',1)
-      call  gop(sarea1,wtmp,'+  ',1)
-
-      flux_moose=sint1/sarea1
-
-      return
-      end
-c-----------------------------------------------------------------------
       subroutine nek_init_step
 
       include 'SIZE'
@@ -367,3 +327,235 @@ C=======================================================================
 
       return
       end
+c-----------------------------------------------------------------------
+      subroutine rassign (fu,vpf)
+      include 'SIZE'
+      include 'TOTAL'
+      include 'NEKMOOSE'
+      integer e,eg
+
+      real*8 vpf(lsurf_m*4)
+      real*8 fu(3,3,3,lelt)
+
+c     Filling a lx1=3 brick for each face from MOOSE. Interpolating 
+c     mid-size points at the moment. 
+      
+      do e=1,nelt
+
+      do i=1,27
+         fu(i,1,1,e)=0.0
+      enddo 
+
+      if (plmask(e).eq.1)  then
+
+      i_us=(pplist(e)-1)*4
+
+      if (pflmask(e).eq.1) then
+      fu(1,1,1,e) = vpf(i_us+1)
+      fu(3,1,1,e) = vpf(i_us+2)
+      fu(1,1,3,e) = vpf(i_us+4)
+      fu(3,1,3,e) = vpf(i_us+3)   
+      fu(2,1,1,e) = 0.5*(fu(1,1,1,e)+fu(3,1,1,e)) ! interpolate midpoints 
+      fu(3,1,2,e) = 0.5*(fu(3,1,1,e)+fu(3,1,3,e))  
+      fu(2,1,3,e) = 0.5*(fu(1,1,3,e)+fu(3,1,3,e))
+      fu(1,1,2,e) = 0.5*(fu(1,1,1,e)+fu(1,1,3,e))
+      fu(2,1,2,e) = 
+     &  0.25*(fu(2,1,1,e)+fu(3,1,2,e)+fu(2,1,3,e)+fu(1,1,2,e)) 
+      do i=1,3         ! fill entire brick with surface solution to enable spectral interpolation
+        do j=1,3
+           fu(i,2,j,e)=fu(i,1,j,e)
+           fu(i,3,j,e)=fu(i,1,j,e) 
+        enddo
+      enddo 
+      endif
+
+      if (pflmask(e).eq.2) then
+      fu(3,1,1,e) = vpf(i_us+1)
+      fu(3,3,1,e) = vpf(i_us+2)
+      fu(3,1,3,e) = vpf(i_us+4)
+      fu(3,3,3,e) = vpf(i_us+3)
+      fu(3,2,1,e) = 0.5*(fu(3,1,1,e)+fu(3,3,1,e)) ! interpolate midpoints 
+      fu(3,3,2,e) = 0.5*(fu(3,3,1,e)+fu(3,3,3,e))
+      fu(3,2,3,e) = 0.5*(fu(3,1,3,e)+fu(3,3,3,e))
+      fu(3,1,2,e) = 0.5*(fu(3,1,1,e)+fu(3,1,3,e))
+      fu(3,2,2,e) =
+     &  0.25*(fu(3,2,1,e)+fu(3,3,2,e)+fu(3,2,3,e)+fu(3,1,2,e))
+      do i=1,3
+        do j=1,3        ! fill entire brick with surface solution to enable spectral interpolation
+  
+           fu(2,i,j,e)=fu(3,i,j,e)
+           fu(1,i,j,e)=fu(3,i,j,e)
+        enddo
+      enddo
+      endif
+
+      if (pflmask(e).eq.3)  then
+      fu(1,3,1,e) = vpf(i_us+1)
+      fu(3,3,1,e) = vpf(i_us+2)
+      fu(1,3,3,e) = vpf(i_us+4)
+      fu(3,3,3,e) = vpf(i_us+3)
+      fu(2,3,1,e) = 0.5*(fu(1,3,1,e)+fu(3,3,1,e)) ! interpolate midpoints 
+      fu(3,3,2,e) = 0.5*(fu(3,3,1,e)+fu(3,3,3,e))
+      fu(2,3,3,e) = 0.5*(fu(1,3,3,e)+fu(3,3,3,e))
+      fu(1,3,2,e) = 0.5*(fu(1,3,1,e)+fu(1,3,3,e))
+      fu(2,3,2,e) =
+     &  0.25*(fu(2,3,1,e)+fu(3,3,2,e)+fu(2,3,3,e)+fu(1,3,2,e))
+      do i=1,3         ! fill entire brick with surface solution to enable spectral interpolation
+        do j=1,3
+           fu(i,1,j,e)=fu(i,3,j,e)
+           fu(i,2,j,e)=fu(i,3,j,e)
+        enddo
+      enddo 
+      endif
+
+      if (pflmask(e).eq.4) then
+      fu(1,1,1,e) = vpf(i_us+1)
+      fu(1,3,1,e) = vpf(i_us+2)
+      fu(1,1,3,e) = vpf(i_us+4)
+      fu(1,3,3,e) = vpf(i_us+3)
+      fu(1,2,1,e) = 0.5*(fu(1,1,1,e)+fu(1,3,1,e)) ! interpolate midpoints 
+      fu(1,3,2,e) = 0.5*(fu(1,3,1,e)+fu(1,3,3,e))
+      fu(1,2,3,e) = 0.5*(fu(1,1,3,e)+fu(1,3,3,e))
+      fu(1,1,2,e) = 0.5*(fu(1,1,1,e)+fu(1,1,3,e))
+      fu(1,2,2,e) =
+     &  0.25*(fu(1,2,1,e)+fu(1,3,2,e)+fu(1,2,3,e)+fu(1,1,2,e))
+      do i=1,3
+        do j=1,3        ! fill entire brick with surface solution to enable spectral interpolation
+           fu(2,i,j,e)=fu(1,i,j,e)
+           fu(3,i,j,e)=fu(1,i,j,e)
+        enddo
+      enddo
+      endif
+
+      if (pflmask(e).eq.5) then
+      fu(1,1,1,e) = vpf(i_us+1)
+      fu(3,1,1,e) = vpf(i_us+2)
+      fu(1,3,1,e) = vpf(i_us+4)
+      fu(3,3,1,e) = vpf(i_us+3)
+      fu(2,1,1,e) = 0.5*(fu(1,1,1,e)+fu(3,1,1,e)) ! interpolate midpoints 
+      fu(3,2,1,e) = 0.5*(fu(3,1,1,e)+fu(3,3,1,e))
+      fu(2,3,1,e) = 0.5*(fu(1,3,1,e)+fu(3,3,1,e))
+      fu(1,2,1,e) = 0.5*(fu(1,1,1,e)+fu(1,3,1,e))
+      fu(2,2,1,e) =
+     &  0.25*(fu(2,1,1,e)+fu(3,2,1,e)+fu(2,3,1,e)+fu(1,2,1,e))
+      do i=1,3
+        do j=1,3        ! fill entire brick with surface solution to enable spectral interpolation
+           fu(i,j,2,e)=fu(i,j,1,e)
+           fu(i,j,3,e)=fu(i,j,1,e)
+        enddo
+      enddo
+      endif
+
+      if (pflmask(e).eq.6) then
+      fu(1,1,3,e) = vpf(i_us+1)
+      fu(3,1,3,e) = vpf(i_us+2)
+      fu(1,3,3,e) = vpf(i_us+4)
+      fu(3,3,3,e) = vpf(i_us+3)
+      fu(2,1,3,e) = 0.5*(fu(1,1,3,e)+fu(3,1,3,e)) ! interpolate midpoints 
+      fu(3,2,3,e) = 0.5*(fu(3,1,3,e)+fu(3,3,3,e))
+      fu(2,3,3,e) = 0.5*(fu(1,3,3,e)+fu(3,3,3,e))
+      fu(1,2,3,e) = 0.5*(fu(1,1,3,e)+fu(1,3,3,e))
+      fu(2,2,3,e) =
+     &  0.25*(fu(2,1,3,e)+fu(3,2,3,e)+fu(2,3,3,e)+fu(1,2,3,e))
+      do i=1,3
+        do j=1,3        ! fill entire brick with surface solution to enable spectral interpolation
+           fu(i,j,1,e)=fu(i,j,3,e)
+           fu(i,j,2,e)=fu(i,j,3,e)
+        enddo
+      enddo
+      endif
+
+      endif
+      enddo  
+
+      return
+      end
+C=======================================================================
+      subroutine int_surf12(fo,fi,n2,n1,ne)
+      include 'SIZE'
+
+      parameter(lx=20)
+      real*8 z1(lx),z2(lx),w(lx)
+      real*8 fm12(lx*lx),fm12t(lx*lx)
+      real*8 work(lx*lx*lx)
+      real*8 fo(n2,n2,n2,lelt), fi(n1,n1,n1,lelt)
+      integer n1, n2, e, ne
+
+      if (n1.gt.lx.or.n2.gt.lx) then
+         write(6,*)'ERROR: increase lx in setmap to max:',n1,n2
+         call exitt
+      endif
+
+      call rzero(fo,n2*n2*n2*ne)
+
+      call zwgll(z1,w,n1)
+      call zwgll(z2,w,n2)
+      call igllm(fm12,fm12t,z1,z2,n1,n2,n1,n2)
+
+      do e=1,ne
+      call specmpf(fo(1,1,1,e),n2,fi(1,1,1,e),n1,fm12,fm12t,work)
+      enddo
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine specmpf(b,nb,a,na,ba,ab,w)
+C
+C     -  Spectral interpolation from A to B via tensor products
+C     -  scratch arrays: w(na*na*nb)
+C
+C
+      include 'SIZE'
+      include 'INPUT'
+      real b(nb,nb,nb),a(na,na,na)
+      real w(1)
+C
+
+         nab = na*nb
+         nbb = nb*nb
+         call mxm(ba,nb,a,na,b,na*na)
+         k=1
+         l=1
+         do iz=1,na
+            call mxm(b(k,1,1),nb,ab,na,w(l),nb)
+            k=k+nab
+            l=l+nbb
+         enddo
+         call mxm(w,nbb,ab,na,b,nb)
+      return
+      end
+C=======================================================================
+      subroutine flux_reconstruction()
+      include 'SIZE'
+      include 'TOTAL'
+      include 'NEKMOOSE'
+      common /flux_reconstructed/flux_recon(lx1,ly1,lz1,lelt)
+      integer e,f,lx_low
+      real*8  flux_low(3,3,3,lelt) 
+      real*8  flux_recon
+      ntot=nx1*ny1*nz1*nelt
+!     Call to rassign and spectral interpolation
+
+      call rassign(flux_low,pc_f)
+  
+      lx_low=3
+      call int_surf12(flux_recon,flux_low,lx1,lx_low,lelt) 
+
+c      do e=1,nelt
+c        do f=1,6
+c          call surface_int(sint,sarea,flux_recon,e,f)
+c          if (cbc(f,e,1).eq.'W  ') then
+c           sint1=sint1+sint
+c           sarea1=sarea1+sarea
+c          endif
+c         enddo
+c      enddo
+
+c      call  gop(sint1,wtmp,'+  ',1)
+c      call  gop(sarea1,wtmp,'+  ',1)
+
+c      flux_moose=sint1/sarea1
+
+      return
+      end
+c-----------------------------------------------------------------------
